60c5fbf9a91599c07dd211ecc6f27e592377b933
Jack Millard <millard64@hotmail.co.uk>
2022-08-05 18:29:58 +0100

    Add comments for table usage

--- lib/deploy/table.rb
+++ lib/deploy/table.rb
@@ -18,21 +18,32 @@ module Deploy
       )
     end
 
+    def padding(*pads)
+      @padding = pads.length == 1 ? pads.first : pads
+    end
+
+    # Each of `headers` and `rows` should be called like:
+    # table.headers('First', 'Second')
+    #
+    # The `*args` argument uses the splat operator (*), which takes
+    # all arguments passed to the method and squashes them into a 
+    # single array. The above method call would expose the array
+    # `titles` to the body of `headers`, with the contents:
+    # `['First', 'Second']
+
     def headers(*titles)
       titles.each_with_index do |title, i|
         @table.header[i] = title
       end
     end
 
-    def padding(*pads)
-      @padding = pads.length == 1 ? pads.first : pads
-    end
-
+    # Add single row from single array
     def row(*vals)
       @table << vals
     end
     
-    def row(*vals)
+    # Add multiple rows from nested array
+    def rows(*vals)
       vals.each do |r|
         @table << r
       end
