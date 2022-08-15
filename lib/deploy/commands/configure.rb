8f45a0a5404547608f04bc253621242923455c85
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-08-15 17:28:02 +0100

    Asks if details given are correct and reasks questions if not

--- lib/deploy/commands/configure.rb
+++ lib/deploy/commands/configure.rb
@@ -5,13 +5,27 @@ module Deploy
   module Commands
     class Configure < Command
       def run
-        prompt = TTY::Prompt.new
-        result = prompt.collect do
+        ask = true
+        while ask
+          ask_questions
+          ask = false if prompt.yes?("\nAre these details correct?")
+        end
+        ENV['CLUSTER_NAME'] = @result[:name]
+        ENV['IP_RANGE'] = @result[:ip]
+      end
+
+      private
+
+      def ask_questions
+        puts "\n"
+        @result = prompt.collect do
           key(:name).ask('Cluster name:', default: 'my-cluster')
           key(:ip).ask('IP range:', required: true)
         end
-        ENV['CLUSTER_NAME'] = result[:name]
-        ENV['IP_RANGE'] = result[:ip]
+      end
+
+      def prompt
+        @prompt ||= TTY::Prompt.new(help_color: :yellow)
       end
     end
   end
