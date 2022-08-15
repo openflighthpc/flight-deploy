6f4afe9761ce723ba06cade1bd46b9f66b0b54f6
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-08-15 13:11:41 +0100

    Get name and IP range from tty prompt and set env variables

--- lib/deploy/commands/configure.rb
+++ lib/deploy/commands/configure.rb
@@ -1,10 +1,17 @@
+require 'tty-prompt'
 require_relative '../command'
 
 module Deploy
   module Commands
     class Configure < Command
       def run
-        puts 'Hey'
+        prompt = TTY::Prompt.new
+        result = prompt.collect do
+          key(:name).ask('Cluster name:', default: 'my-cluster')
+          key(:ip).ask('IP range:', required: true)
+        end
+        ENV['CLUSTER_NAME'] = result[:name]
+        ENV['IP_RANGE'] = result[:ip]
       end
     end
   end
