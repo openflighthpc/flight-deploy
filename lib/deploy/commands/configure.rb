b48c4a6bfc977dfa0fdadb43d5724805f264e160
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-08-16 12:27:48 +0100

    Refactor to move loading and saving config to Config class

--- lib/deploy/commands/configure.rb
+++ lib/deploy/commands/configure.rb
@@ -28,13 +28,13 @@ module Deploy
       def ask_questions
         puts "\n"
         @answers = prompt.collect do
-          key(:name).ask('Cluster name:') do |q|
+          key(:cluster_name).ask('Cluster name:') do |q|
             q.default 'my-cluster'
             q.validate /^[a-zA-Z0-9_\-]+$/
             q.messages[:valid?] = "Invalid cluster name: %{value}. " \
               "Must contain only alphanumeric characters, '-' and '_'."
           end
-          key(:ip).ask('IP range:') do |q|
+          key(:ip_range).ask('IP range:') do |q|
             q.required true
             q.validate /^[0-9\/.]+$/
             q.messages[:valid?] = "Invalid IP range: %{value}. " \
@@ -49,10 +49,7 @@ module Deploy
 
       def save_answers
         raise 'Attempted to save answers without answering questions' unless @answers
-        config = YAML.load_file(Config.config_path)
-        config['cluster_name'] = @answers[:name]
-        config['ip_range'] = @answers[:ip].to_s
-        File.write(Config.config_path, YAML.dump(config))
+        Config.append_to_config(@answers)
       end
     end
   end
