631e4afd3ffbdf8262315f35cc0a5e9963eaf63e
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-08-17 16:01:59 +0100

    Refactor view command
    Use heredoc instead of many puts
    Change status methods to constants

--- lib/deploy/commands/view.rb
+++ lib/deploy/commands/view.rb
@@ -3,16 +3,26 @@ require_relative '../command'
 module Deploy
   module Commands
     class View < Command
+      SUCCESS_STATUSES = %w[ok changed rescued]
+      FAIL_STATUSES = %w[failed fatal unreachable]
+      SKIP_STATUSES = %w[skipped ignored]
+      ALL_STATUSES = SUCCESS_STATUSES + FAIL_STATUSES + SKIP_STATUSES
+
       def run
         @hostname = args[0]
         raise "Setup has not been run for this node." unless node.log_filepath
-        puts "\nRunning:"
-        puts "   #{node.command}"
-        puts "\nProgress:"
-        display_task_status
-        puts "\nStatus:"
-        puts "   #{node.status.upcase}"
-        puts "\n"
+        puts <<HEREDOC
+
+Running:
+   #{node.command.chomp}
+
+Progress:
+#{display_task_status.chomp}
+
+Status:
+   #{node.status.upcase}
+
+HEREDOC
       end
 
       def node
@@ -22,47 +32,34 @@ module Deploy
       def display_task_status
         task_name, task_status, role, new_role = nil
         roles = []
+        str = ""
         node.log_file.readlines.each do |line|
           if @options.raw
-            puts line unless line == node.command
+            str += line unless line == node.command
           else
             if line.start_with?('TASK')
               role, task_name = line[ /\[(.*?)\]/, 1 ].split(' : ')
               new_role = !roles.include?(role)
               roles << role if new_role
-            elsif all_statuses.any? { |s| line.start_with?(s) }
-              if !task_status || success_statuses.include?(task_status)
+            elsif ALL_STATUSES.any? { |s| line.start_with?(s) }
+              if !task_status || SUCCESS_STATUSES.include?(task_status)
                 task_status = line.split(':')
                                   .first
               end
             elsif task_name && task_status
-              puts role if new_role
-              if success_statuses.include?(task_status)
-                puts "   \u2705 #{task_name}"
-              elsif fail_statuses.include?(task_status)
-                puts "   \u274c #{task_name}"
+              str += "#{role}\n" if new_role
+              if SUCCESS_STATUSES.include?(task_status)
+                str += "   \u2705 #{task_name}\n"
+              elsif FAIL_STATUSES.include?(task_status)
+                str += "   \u274c #{task_name}\n"
               end
               task_name, task_status = nil
             end
           end
         end
+        str
       end
 
-      def success_statuses
-        %w[ok changed rescued]
-      end
-
-      def fail_statuses
-        %w[failed fatal unreachable]
-      end
-
-      def skip_statuses
-        %w[skipped ignored]
-      end
-
-      def all_statuses
-        success_statuses + fail_statuses + skip_statuses
-      end
     end
   end
 end
