4291d51f30d570158489896af96d68e118b9a11f
VoxSecundus <millard64@hotmail.co.uk>
2022-08-17 15:29:30 +0100

    Plug profile details into setup command

--- lib/deploy/inventory.rb
+++ lib/deploy/inventory.rb
@@ -2,42 +2,45 @@ require_relative './config'
 
 module Deploy
   class Inventory
-    def self.load(name:)
+    def self.load(name=nil)
       begin
         file = File.read(File.join(Config.ansible_inv_dir, "#{name}.inv"))
         groups = file.split(/(?=\[.*\])/).reject(&:empty?)
 
-        inventory = groups.map do |r|
+        inventory = {}
+        groups.each do |r|
           group = r.split("\n")
-          { group: group[0][1...-1], nodes: group[1..-1] }
+          inventory[group[0][1...-1]] = group[1..-1]
         end
 
-        new(name, inventory)
+        new(cluster_name: name, groups: inventory)
       rescue Errno::ENOENT
-        new(name, []).tap do |inv|
+        new(cluster_name: name, groups: {}).tap do |inv|
           inv.dump
         end
       end
     end
 
     def to_raw
-      groups.map do |row|
-        nodes_str = row[:nodes].join("\n")
-        "[#{row[:group]}]#{nodes_str}"
+      groups.map do |group, nodes|
+        nodes_str = nodes.join("\n")
+        "[#{group}]\n#{nodes_str}"
       end.join("\n\n")
     end
 
     def dump
-      File.open(filepath, 'w+') { |f| to_raw }
+      File.open(filepath, 'w+') { |f| f.write(to_raw) }
     end
 
     def filepath
-      File.join("var", "#{cluster_name}.yaml")
+      File.join(Config.ansible_inv_dir, "#{cluster_name}.inv")
     end
+
+    attr_accessor :cluster_name, :groups
 
     def initialize(cluster_name:, groups:)
       @cluster_name = cluster_name
-      @groups: groups
+      @groups = groups
     end
   end
 end
