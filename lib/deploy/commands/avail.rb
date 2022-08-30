ee21fb6554152c0d033e4eae0e02e87c1eba6324
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-08-30 16:11:07 +0100

    Display table with type names and descriptions

--- lib/deploy/commands/avail.rb
+++ lib/deploy/commands/avail.rb
@@ -1,10 +1,18 @@
 require_relative '../command'
+require_relative '../type'
 
 module Deploy
   module Commands
     class Avail < Command
       def run
-        puts "Hey"
+        raise "No available cluster types" unless Type.all.any?
+
+        t = Table.new
+        t.headers('Name', 'Description')
+        Type.all.each do |p|
+          t.row( p.name, p.description )
+        end
+        t.emit
       end
     end
   end
