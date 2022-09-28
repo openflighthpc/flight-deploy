ac5e6eb683e82451723ff8af345b4efb67409355
VoxSecundus <millard64@hotmail.co.uk>
2022-09-28 16:18:16 +0100

    Bump version rc

--- lib/profile/version.rb
+++ lib/profile/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-desktop
 # ==============================================================================
 module Profile
-  VERSION = '0.0.3'
+  VERSION = '0.0.4-rc1'
 end
