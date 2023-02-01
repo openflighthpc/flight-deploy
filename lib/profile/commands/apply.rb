02b4b7c21035f6bbb3d23beb56dbb46b09d44595
VoxSecundus <millard64@hotmail.co.uk>
2023-02-01 14:41:35 +0000

    Use Enumerator#map instead of #each

--- lib/profile/commands/apply.rb
+++ lib/profile/commands/apply.rb
@@ -204,29 +204,25 @@ module Profile
       end
       
       def expand_brackets(str)
-        labels = []
         contents = str[/\[.*\]/] 
-        if contents.nil?
-          labels = [str]
-        else
-          left = str[/[^\[]*/]
-          right = str[/].*/][1..-1]
-          
-          unless contents.match(/^\[[0-9]+-[0-9]+\]$/)
-            raise "Invalid range, ensure any range used is of the form [START-END]"
-          end
-          
-          nums = contents[1..-2].split("-")
-          
-          unless nums.first.to_i < nums.last.to_i
-            raise "Invalid range, ensure that the end index is greater than the start index"
-          end
-          
-          (nums.first..nums.last).each do |index|
-            labels.append(left + index + right)
-          end
+        return [str] if contents.nil?
+
+        left = str[/[^\[]*/]
+        right = str[/].*/][1..-1]
+
+        unless contents.match(/^\[[0-9]+-[0-9]+\]$/)
+          raise "Invalid range, ensure any range used is of the form [START-END]"
         end
-        labels
+
+        nums = contents[1..-2].split("-")
+
+        unless nums.first.to_i < nums.last.to_i
+          raise "Invalid range, ensure that the end index is greater than the start index"
+        end
+
+        (nums.first..nums.last).map do |index|
+          left + index + right
+        end
       end
     end
   end
