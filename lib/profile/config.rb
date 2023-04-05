c28605aec454c1602ecac5bf884d9515af86d75e
VoxSecundus <millard64@hotmail.co.uk>
2023-04-05 12:57:13 +0100

    Refactor config to use XDG/TTY::Config

--- lib/profile/config.rb
+++ lib/profile/config.rb
@@ -1,33 +1,48 @@
 require 'fileutils'
 require 'pathname'
 require 'yaml'
-require 'shash'
+require 'xdg'
+require 'tty-config'
 
 module Profile
   class Config
     class << self
+      PROFILE_DIR_SUFFIX = File.join('flight', 'profile')
 
-      # Loads in config file as a hash
-      # Convert to super hash so any YAML keys in the file
-      # can be accessed like a regular method
-      def config
-        @config ||= config_hash.to_shash
-      rescue NoMethodError
-        raise "Config file has missing values"
+      def data
+        @data ||= TTY::Config.new.tap do |cfg|
+          cfg.append_path(File.join(root, 'etc'))
+          begin
+            cfg.read
+          rescue TTY::Config::ReadError
+            nil
+          end
+        end
       end
 
       def cluster_type
-        config.cluster_type
+        data.fetch(:cluster_type)
       end
 
       def use_hunter?
-        config.use_hunter || false
+        data.fetch(:use_hunter) || false
       end
 
+      def type_paths
+        data.fetch(:type_paths) || [File.join(root, "etc/types")]
+      end
+
+<<<<<<< Updated upstream
+=======
+      def log_dir
+        data.fetch(:log_dir) || dir_constructor(root, 'var', 'log')
+      end
+
+>>>>>>> Stashed changes
       def hunter_command
         command = 
           ENV['flight_PROFILE_hunter_command'] ||
-            config.hunter_command ||
+            data.fetch(:hunter_command) ||
             File.join(ENV.fetch('flight_ROOT', '/opt/flight/'), 'bin/flight hunter')
         if !File.file?(File.join(command.split[0]))
           raise "Could not find '#{command.split[0]}'"
@@ -41,50 +56,29 @@ module Profile
         ENV['PATH']
       end
 
-      def config_hash
-        @config_hash ||= File.exists?(config_path) ? (YAML.load_file(config_path) || {}) : {}
-      end
-
-      def append_to_config(details_to_append)
-        details_to_append.each do |key,value|
-          config_hash[key] = value
-        end
-        File.write(config_path, YAML.dump(config_hash))
-      end
-
-      def fetch(*keys)
-        values = keys.map do |key|
-          config.public_send(key.to_sym)
-        end
-        values.length > 1 ? values : values.first
-      end
-
-      def root
-        @root ||= File.expand_path('../..', __dir__)
-      end
-
-      def answers_dir
-        dir_constructor(root, 'var', 'answers/')
-      end
-
-      def config_path
-        File.join(root, "etc/config.yml")
-      end
-
       def inventory_dir
         dir_constructor(root, "var", "inventory")
-      end
-
-      def log_dir
-        File.join(root, "log/")
       end
 
       def ansible_inv_dir
         dir_constructor(root, "var", "ansible_invs")
       end
 
-      def type_paths
-        (config.type_paths || [File.join(root, "etc/types")])
+      def answers_dir
+        dir_constructor(root, 'var', 'answers/')
+      end
+
+      def log_dir
+        File.join(root, "log/")
+      end
+
+      def save_data
+        FileUtils.mkdir_p(File.join(root, 'etc'))
+        data.write(force: true)
+      end
+
+      def root
+        @root ||= File.expand_path(File.join(__dir__, '..', '..'))
       end
 
       private
@@ -92,6 +86,18 @@ module Profile
       def dir_constructor(*a)
         dir = File.join(*a) 
         FileUtils.mkdir_p(dir).first
+      end
+
+      def xdg_config
+        @xdg_config ||= XDG::Config.new
+      end
+
+      def xdg_data
+        @xdg_data ||= XDG::Data.new
+      end
+
+      def xdg_cache
+        @xdg_cache ||= XDG::Cache.new
       end
     end
   end
