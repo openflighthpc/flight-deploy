b7c453a57624de0ce5cc8ec053aaaf98f8c37e19
Matt Ruddy <matt@calcwise.com>
2023-06-28 18:47:21 +0100

    Run smart default commands concurrently

--- lib/profile/commands/configure.rb
+++ lib/profile/commands/configure.rb
@@ -49,11 +49,14 @@ module Profile
 
         prefills = {}
         type.questions.each do |question|
-          prefills[question.id] = generate_prefill(question)
+          Process.fork do
+            prefills[question.id] = generate_prefill(question)
+          end
         end
         prompt.collect do
           type.questions.each do |question|
             key(question.id).ask(question.text) do |q|
+              sleep(0.25) until prefills[question.id]
               q.default prefills[question.id]
               q.required question.validation.required
               if question.validation.to_h.key?(:format)
