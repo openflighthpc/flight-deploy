93610a2180c447408e870af6a3bd8ddcf6c08dfa
VoxSecundus <millard64@hotmail.co.uk>
2023-08-01 16:25:22 +0000

    Add success message to dequeue command

--- lib/profile/commands/dequeue.rb
+++ lib/profile/commands/dequeue.rb
@@ -1,12 +1,14 @@
 require_relative '../command'
+require_relative './concerns/node_utils'
 require_relative '../config'
 require_relative '../table'
 require_relative '../node'
 module Profile
   module Commands
     class Dequeue < Command
-      def run
+      include Concerns::NodeUtils
 
+      def run
         strings = args[0].split(',')
         names = []
         strings.each do |str|
@@ -17,39 +19,17 @@ module Profile
 
         failed = []
         names.each do |name|
-          node_file = File.join(Config.queue_dir, name)
-          if File.exists?(node_file)
-            File.delete(node_file)
+          if QueueManager.contains?(name)
+            QueueManager.pop(name)
           else
             failed << name
           end
         end
 
+        removed = names - failed
+        puts "Nodes removed from queue: #{removed.join(', ')}" unless removed.empty?
+
         raise "The following nodes were not found in the queue: #{failed.join(', ')}" unless failed.empty?
-      end
-
-      private
-
-      def expand_brackets(str)
-        contents = str[/\[.*\]/]
-        return [str] if contents.nil?
-
-        left = str[/[^\[]*/]
-        right = str[/].*/][1..-1]
-
-        unless contents.match(/^\[[0-9]+-[0-9]+\]$/)
-          raise "Invalid range, ensure any range used is of the form [START-END]"
-        end
-
-        nums = contents[1..-2].split("-")
-
-        unless nums.first.to_i < nums.last.to_i
-          raise "Invalid range, ensure that the end index is greater than the start index"
-        end
-
-        (nums.first..nums.last).map do |index|
-          left + index + right
-        end
       end
     end
   end
