c92824c6555d89a07217f4719df7c23974cbffe7
VoxSecundus <millard64@hotmail.co.uk>
2023-08-03 10:30:00 +0100

    Lint queue manager logic

--- lib/profile/queue_manager.rb
+++ lib/profile/queue_manager.rb
@@ -3,12 +3,12 @@ module Profile
     def self.push(name, identity)
       Queue.push(name, identity)
 
-      unless QueueMonitor.running?
-        # Fork process and daemonise it so that parent process can continue
-        Process.fork do
-          Process.daemon
-          QueueMonitor.start
-        end
+      return if QueueMonitor.running?
+
+      # Fork process and daemonise it so that parent process can continue
+      Process.fork do
+        Process.daemon
+        QueueMonitor.start
       end
     end
 
@@ -20,80 +20,82 @@ module Profile
       Queue.contains?(name)
     end
 
-    private
+    def self.identity(name)
+      Queue.index[name.to_sym][:identity]
+    end
+  end
 
-    # Not to be confused with Thread::Queue
-    class Queue
-      class << self
-        def node_file(name)
-          File.join(Config.queue_dir, name)
-        end
+  # Not to be confused with Thread::Queue
+  class Queue
+    class << self
+      def node_file(name)
+        File.join(Config.queue_dir, name)
+      end
 
-        def push(name, identity)
-          File.open(node_file(name), 'w') do |file|
-            file.write(identity)
-          end
-        end
-
-        def pop(*names)
-          names.each do |name|
-            next unless contains?(name)
-            File.delete(node_file(name))
-          end
-        end
-
-        def contains?(name)
-          File.exists?(node_file(name))
-        end
-
-        def index
-          Dir[node_file('*')].map do |f|
-            { name: File.basename(f), identity: File.read(f) }
-          end
+      def push(name, identity)
+        File.open(node_file(name), 'w') do |file|
+          file.write(identity)
         end
       end
+
+      def pop(*names)
+        names.each do |name|
+          next unless contains?(name)
+
+          File.delete(node_file(name))
+        end
+      end
+
+      def contains?(name)
+        File.file?(node_file(name))
+      end
+
+      def index
+        Dir[node_file('*')].map { |f| [File.basename(f), File.read(f)] }.to_h
+      end
     end
+  end
 
-    class QueueMonitor
-      class << self
-        def pidfile
-          Config.queue_pidfile
-        end
+  class QueueMonitor
+    class << self
+      def pidfile
+        Config.queue_pidfile
+      end
 
-        def pid
-          running? && File.read(pidfile)
-        end
+      def pid
+        running? && File.read(pidfile)
+      end
 
-        def running?
-          File.file?(pidfile)
-        end
+      def running?
+        File.file?(pidfile)
+      end
 
-        def start
-          File.write(pidfile, Process.pid)
+      def start
+        File.write(pidfile, Process.pid)
 
-          until Queue.index.empty?
-            grouped = Queue.index.group_by { |n| n[:identity] }
-            grouped.each do |group, nodes|
-              names = nodes.map { |n| n[:name] }
+        until Queue.index.empty?
+          grouped = Queue.index.group_by { |k,v| v }
+          grouped.each do |group, nodes|
+            names = nodes.map(&:last)
 
-              Queue.pop(names)
+            Queue.pop(names)
 
-              args = [
-                names.join(','),
-                group
-              ]
+            args = [
+              names.join(','),
+              group
+            ]
 
-              begin
-                Commands::Apply.new(args, OpenStruct.new).run!
-              rescue
-              end
+            begin
+              Commands::Apply.new(args, OpenStruct.new).run!
+            rescue => e
+              puts e
             end
-
-            sleep(5)
           end
-        ensure
-          File.delete(pidfile) if File.file?(pidfile)
+
+          sleep(5)
         end
+      ensure
+        File.delete(pidfile) if File.file?(pidfile)
       end
     end
   end
