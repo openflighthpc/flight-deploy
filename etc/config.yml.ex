53b05f1ebf54eb409edf6fb3cd561d843a2a5239
Marco Luo <mailforrgscid@gmail.com>
2023-09-15 11:53:08 +0100

    config.yml.ex completion

--- etc/config.yml.ex
+++ etc/config.yml.ex
@@ -3,11 +3,25 @@
 #type_paths:
 #  -
 #  -
-# Enable the usage of nodes found by hunter (EXPERIMENTAL)
-# Requires `hunter_command` to be set
-#use_hunter:
+
 # Path to Flight Hunter executable
 #hunter_command:
 
+# Enable the usage of nodes found by hunter (EXPERIMENTAL)
+# Requires `hunter_command` to be set
+#use_hunter:
+
 # Secret to use when encrypting strings
 #shared_secret_path:
+
+# The cluster type to be configured
+#cluster_type:
+
+# Path to which the log file is saved
+#log_dir:
+
+# Enable automatic node removal from hunter list after being applied
+#remove_hunter_entry:
+
+# remove the node from the cluster on instance shutdown
+#remove_on_shutdown
