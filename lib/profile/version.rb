fd67ed032a6fc03fc77a8f0c5f9bbdcee32eb911
Jack Millard <millard64@hotmail.co.uk>
2024-01-30 13:49:10 +0000

    Bump version

--- lib/profile/version.rb
+++ lib/profile/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-desktop
 # ==============================================================================
 module Profile
-  VERSION = '1.0.0'
+  VERSION = '1.1.0'
 end
